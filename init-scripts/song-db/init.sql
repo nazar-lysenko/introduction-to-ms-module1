541b5fb059dede9d22aad4f2f46b8ee944670dc1
Nazar_Lysenko <nazar_lysenko@epam.com>
2026-06-11 11:30:14 +0200

    Module 2: Added db init scripts. Added Dockerfile for both services. Updated env and application properties. Updated compose file to include both services.

---
+++ init-scripts/song-db/init.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS songs (
+    id       BIGINT PRIMARY KEY,
+    name     VARCHAR(255),
+    artist   VARCHAR(255),
+    album    VARCHAR(255),
+    duration VARCHAR(255),
+    year     VARCHAR(255)
+);
